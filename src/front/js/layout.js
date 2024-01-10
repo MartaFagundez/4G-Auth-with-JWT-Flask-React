cc01d62727f9700c9eb0b62106d4f9f97bea930a
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-01-10 12:59:57 +0000

    refactor(layout): stylize and add userContext

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,11 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
+import { UserContextProvider } from "./contexts/userContext.js";
+
 import { Home } from "./pages/home";
 import Login from "./pages/login.jsx";
 import Signup from "./pages/signup.jsx";
 import Profile from "./pages/profile.jsx";
-import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
@@ -21,22 +22,24 @@ const Layout = () => {
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
     return (
-        <div>
+        <div className="d-flex flex-column min-vh-100 p-0 m-0 ">
             <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Signup />} path="/signup" />
-                        <Route element={<Login />} path="/login" />
-                        <Route element={<Profile />} path="/profile" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
+                <UserContextProvider>
+                    <ScrollToTop>
+                        <Navbar />
+                        <Routes>
+                            <Route element={<Home />} path="/" />
+                            <Route element={<Signup />} path="/signup" />
+                            <Route element={<Login />} path="/login" />
+                            <Route element={<Profile />} path="/profile" />
+                            <Route element={<h1>Not found!</h1>} />
+                        </Routes>
+                        <Footer />
+                    </ScrollToTop>
+                </UserContextProvider>
             </BrowserRouter>
         </div>
     );
 };
 
-export default injectContext(Layout);
+export default Layout;
