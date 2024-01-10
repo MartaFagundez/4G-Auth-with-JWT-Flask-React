e8c58c6ac71ccc71bf23847db1579f0837d3883c
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-01-10 12:34:36 +0000

    refactor(footer): align to end of viewport

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-3 text-center justify-self-end">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a href="https://www.linkedin.com/in/martafagundezrodriguez">Marta Fagúndez</a>
