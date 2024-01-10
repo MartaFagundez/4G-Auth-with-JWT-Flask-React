2d6889639df65f96b07862a1087c4bc97e367923
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-01-10 00:35:39 +0000

    refactor(api routes): change protected to profile

--- src/app.py
+++ src/app.py
@@ -131,14 +131,14 @@ def login():
 
 
 # ================= Route Protected ================= #
-@app.route("/protected", methods=["GET"])
+@app.route("/profile", methods=["GET"])
 @jwt_required()
-def protected():
+def get_user_profile():
     # Access the identity of the current user with get_jwt_identity
     current_user_id = get_jwt_identity()
     user = User.query.get(current_user_id)
 
-    return jsonify({"id": user.id, "username": user.username}), 200
+    return jsonify({"id": user.id, "username": user.username, "email": user.email}), 200
 
 
 # this only runs if `$ python src/main.py` is executed
