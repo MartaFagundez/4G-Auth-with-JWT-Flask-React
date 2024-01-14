d9c93944c3b2c522526f54b6610a204afb521288
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-01-14 20:30:37 +0000

    feat(signup component): Initial form setup using React Hook Form

--- src/front/js/pages/signup.jsx
+++ src/front/js/pages/signup.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 export default function Signup() {
+  const form = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const { register, control, handleSubmit, formState } = form;
+
+  const { errors } = formState;
+
+
   return (
     <div className='container'>
       <div className="d-flex flex-column align-items-center">
@@ -12,25 +27,27 @@ export default function Signup() {
           {/* Username */}
           <div className="mb-3">
             <label htmlFor="username" className="form-label">Username</label>
-            <input type="text" className="form-control" id="username" name='username' />
+             {/* Registrar el input en useForm mediante la función "register" */}
+            <input type="text" className="form-control" id="username" {...register("username")} />
           </div>
 
           {/* Email */}
           <div className="mb-3">
             <label htmlFor="email" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="email" name='email' />
+            <input type="email" className="form-control" id="email" {...register("email")} />
           </div>
 
           {/* Password */}
           <div className="mb-3">
             <label htmlFor="pass" className="form-label">Password</label>
-            <input type="password" className="form-control" id="pass" name='pass' />
+            <input type="password" className="form-control" id="pass" {...register("pass")} />
           </div>
 
           <button type="submit" className="btn btn-primary">Signup</button>
         </form>
-
       </div>
+
+      <DevTool control={control} /> {/* set up the dev tool */}
     </div>
   )
 }
