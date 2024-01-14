8b69ee5864ba5ec9e61578d1b9e782b703b808a8
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2024-01-14 20:11:14 +0000

    feat(signup component): add initial structure

--- src/front/js/pages/signup.jsx
+++ src/front/js/pages/signup.jsx
@@ -2,8 +2,35 @@ import React from 'react';
 
 export default function Signup() {
   return (
-    <div className='container-fluid'>
-        <h1>Signup</h1>
+    <div className='container'>
+      <div className="d-flex flex-column align-items-center">
+        <h1 className="text-center my-5">Signup</h1>
+        
+        {/* FORM */}
+        <form className="w-100" style={{maxWidth: "500px"}}>
+
+          {/* Username */}
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">Username</label>
+            <input type="text" className="form-control" id="username" name='username' />
+          </div>
+
+          {/* Email */}
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="email" name='email' />
+          </div>
+
+          {/* Password */}
+          <div className="mb-3">
+            <label htmlFor="pass" className="form-label">Password</label>
+            <input type="password" className="form-control" id="pass" name='pass' />
+          </div>
+
+          <button type="submit" className="btn btn-primary">Signup</button>
+        </form>
+
+      </div>
     </div>
   )
 }
